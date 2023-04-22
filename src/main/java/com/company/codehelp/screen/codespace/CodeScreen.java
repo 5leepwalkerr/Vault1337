2b38275a6eb7ac0a7888798b9072bc8a426d0162
maxxxxxxy <cvb.cvb.2017.cvb@gmail.com>
2023-04-22 21:36:12 +0300

    create Topic entity and connect to db

---
+++ src/main/java/com/company/codehelp/screen/codespace/CodeScreen.java
@@ -0,0 +1,11 @@
+package com.company.codehelp.screen.codespace;
+
+import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("CodeScreen")
+@UiDescriptor("code-screen.xml")
+public class CodeScreen extends Screen {
+
+}
